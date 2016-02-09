5874746f98b0ef23eb19ad0df7a0a937717f0729
Flynn <flynn@datawire.io>
2016-02-09 00:43:23 -0500

    To the Cheese Shop!

--- setup.py
+++ setup.py
@@ -24,11 +24,27 @@ setup(
     author = "Flynn",
     author_email = "flynn@datawire.io",
     description = "Datawire Cloud Tools",
-    license = "PSF",
+    license = "Closed beta. Contact author with requests.",
     keywords = "datawire cloud tools",
     url = "http://datawire.io/",   # project home page, if any
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 
-    # could also include long_description, download_url, classifiers, etc.
+    # could also include download_url, etc.
 
-    long_description = "This is the set of tools for interacting with the Datawire Cloud.",
+    long_description = """BETA SOFTWARE. Contact Datawire before use.
+
+    This is the set of tools for interacting with the Datawire Cloud."""
 )
