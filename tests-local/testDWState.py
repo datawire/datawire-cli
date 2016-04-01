c89a96b6786500e8b8c655767a81e94233b7a622
Flynn <flynn@datawire.io>
2016-04-01 01:16:59 -0400

    Revert "More complete Python test."
    This reverts commit 1fcff12a6e13cb9fc39132e24d5c1d596336cd7d.

--- tests-local/testDWState.py
+++ tests-local/testDWState.py
@@ -1,53 +1,10 @@
-#!python
+from DatawireState import DatawireState
 
-import os
+dwState = DatawireState()
 
-import json
+print("Loading state from %s" % dwState.defaultStatePath())
 
-from DatawireState import DatawireState as qDWState
-from datawire.utils.state import DataWireState as pDWState
+dwState.loadDefaultState()
 
-def jsonify(obj):
-  return json.dumps(obj, indent=4, separators=(',',':'), sort_keys=True)
-
-class TestQuarkDWState (object):
-  def test_defaultStatePath(self):
-    qState = qDWState()
-    pState = pDWState()
-
-    mDefaultPath = os.path.join(os.path.expanduser('~'), '.datawire', 'datawire.json')
-    pDefaultPath = pState.state_path
-
-    assert(qState.defaultStatePath() == mDefaultPath)
-    assert(pDefaultPath == mDefaultPath)
-
-  def test_contents(self):
-    pState = pDWState()
-
-    pJSON = pState.toJSON()
-
-    qState = qDWState()
-    qContents = qState.defaultStateContents()
-    qJSON = jsonify(json.loads(qContents))
-
-    assert(pJSON == qJSON)
-
-  def test_loadState(self):
-    pState = pDWState()
-
-    qState = qDWState()
-    qState.loadDefaultState()
-
-    assert(qState.getCurrentOrgID() == pState.currentOrgID())
-    assert(qState.getCurrentEmail() == pState.currentOrg()['email'])
-
-    pSvcs = pState.currentOrg()['service_tokens'].keys()
-    qSvcs = qState.getCurrentServices()
-
-    assert(jsonify(qSvcs) == jsonify(pSvcs))
-
-    for svc in qSvcs:
-      assert(qState.getCurrentServiceToken(svc) == pState.currentServiceToken(svc))
-
-    print("-- we are [%s]%s" % (qState.getCurrentOrgID(), qState.getCurrentEmail()))
-    print("-- animated: %s" % qState.getCurrentServiceToken('animated'))
+print("-- we are [%s]%s" % (dwState.getCurrentOrgID(), dwState.getCurrentEmail()))
+print("-- animated: %s" % dwState.getCurrentServiceToken('animated'))
